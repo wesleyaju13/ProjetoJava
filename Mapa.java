991cab06af6ebc9fdd63cd78d4073831f2879953
Everton Recchi <porktss@gmail.com>
2017-04-04 02:17:40 -0300

    *Não terminado, atualizando as classes

--- Mapa.java
+++ Mapa.java
@@ -15,10 +15,15 @@ public class Mapa {
     
     private int[ ][ ][ ] array_dimensoes;
     
-    public Mapa(){
-        
-    int[][][] array_dimensoes = new int[40][100][30];
-        
+    public Mapa(int largura, int altura, int profundidade){ // PASSO OS PARAMETROS PARA O CONSTRUTOR, QUEM INSTANCIAR A CLASSE MAPA TEM QUE DIZER A ALTURA, LARGURA E PROFUNDIDADE
+        int[][][] array_dimensoes = new int[largura][altura][profundidade]; // INICIO O ARRAY COM A LARGURA, ALTURA E PROFUNDIDADE PASSADAS
+        for (int i=0; i <= profundidade; i++;) {
+            for (int l=0; l <= altura; l++) {
+                for (int j=0; j <= largura; j++) {
+                    array_dimensoes[j][l][i] = // VOU CLIAR UMA FUNÇÃO NA CLASSE DE PISOS, PARA EU USAR AQUI.
+                }
+            }
+        }
     }
     
 }
